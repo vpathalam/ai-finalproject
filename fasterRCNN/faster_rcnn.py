9ac5c266f7b6e83299380c9edbefef0c03ec0034
Stefan Philip <stefankphilip631@gmail.com>
2020-12-05 20:59:22 -0500

    ground truth anchors

--- fasterRCNN/faster_rcnn.py
+++ fasterRCNN/faster_rcnn.py
@@ -40,6 +40,14 @@ class Config:
         self.use_vertical_flips = False
         self.rot_90 = False
 
+        # pixel channel means used for mean centering at zero
+        self.img_channel_mean = [103.939, 116.779, 123.68] #TODO may need to recalculate using openCV meanStdDev
+        self.img_scaling_factor = 1.0
+
+        # scaling the stdev
+        self.std_scaling = 4.0
+        self.classifier_regr_std = [8.0, 8.0, 4.0, 4.0]
+
 
 # Calculating Intersection over Union (IoU) metric
 def union(au, bu, area_intersection):
@@ -461,4 +469,61 @@ def augment(img_data, config, augment=True):
     img_data_aug['width'] = img.shape[1]
     img_data_aug['height'] = img.shape[0]
     return img_data_aug, img
+
+
+# ground truth anchors as Y for classifier and regressor
+def get_anchor_gt(all_img_data, C, img_length_calc_function, mode='train'):
+    while True:
+
+        for img_data in all_img_data:
+            try:
+
+                # read in image, and optionally add augmentation only for training
+                if mode == 'train':
+                    img_data_aug, x_img = augment(img_data, C, augment=True)
+                else:
+                    img_data_aug, x_img = augment(img_data, C, augment=False)
+
+                (width, height) = (img_data_aug['width'], img_data_aug['height'])
+                (rows, cols, _) = x_img.shape
+
+                assert cols == width
+                assert rows == height
+
+                # get image dimensions for resizing
+                (resized_width, resized_height) = get_new_img_size(width, height, C.im_size)
+
+                # resize the image so that smallest side is length = 300px
+                x_img = cv2.resize(x_img, (resized_width, resized_height), interpolation=cv2.INTER_CUBIC)
+                debug_img = x_img.copy()
+
+                try:
+                    y_rpn_cls, y_rpn_regr, num_pos = calc_rpn(C, img_data_aug, width, height, resized_width,
+                                                              resized_height, img_length_calc_function)
+                except:
+                    continue
+
+                # Zero-center by mean pixel, and preprocess image
+
+                x_img = x_img[:, :, (2, 1, 0)]  # BGR -> RGB
+                x_img = x_img.astype(np.float32)
+                x_img[:, :, 0] -= C.img_channel_mean[0]
+                x_img[:, :, 1] -= C.img_channel_mean[1]
+                x_img[:, :, 2] -= C.img_channel_mean[2]
+                x_img /= C.img_scaling_factor
+
+                x_img = np.transpose(x_img, (2, 0, 1))
+                x_img = np.expand_dims(x_img, axis=0)
+
+                y_rpn_regr[:, y_rpn_regr.shape[1] // 2:, :, :] *= C.std_scaling
+
+                x_img = np.transpose(x_img, (0, 2, 3, 1))
+                y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1))
+                y_rpn_regr = np.transpose(y_rpn_regr, (0, 2, 3, 1))
+
+                yield np.copy(x_img), [np.copy(y_rpn_cls), np.copy(y_rpn_regr)], img_data_aug, debug_img, num_pos
+
+            except Exception as e:
+                print(e)
+                continue
 
