f5af57bfe1ea92ae2f9243fe3218ca8cbeb48f53
Austin Ho <austin@Austins-MacBook-Pro.local>
2020-12-06 17:34:54 -0500

    Hi

--- ssd.py
+++ ssd.py
@@ -1,5 +1,3 @@
-"""Keras implementation of SSD."""
-
 import keras.backend as keras
 import numpy as np
 import tensorflow as tf
@@ -13,7 +11,154 @@ from keras.engine.topology import InputSpec
 from keras.engine.topology import Layer
 
 
+class PriorBox(Layer):
+    """Generate the prior boxes of designated sizes and aspect ratios.
+    # Arguments
+        img_size: Size of the input image as tuple (w, h).
+        min_size: Minimum box size in pixels.
+        max_size: Maximum box size in pixels.
+        aspect_ratios: List of aspect ratios of boxes.
+        flip: Whether to consider reverse aspect ratios.
+        variances: List of variances for x, y, w, h.
+        clip: Whether to clip the prior's coordinates
+            such that they are within [0, 1].
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if dim_ordering='tf'.
+    # Output shape
+        3D tensor with shape:
+        (samples, num_boxes, 8)
+    # References
+        https://arxiv.org/abs/1512.02325
+    """
+    def __init__(self, img_dimension, min_size, max_size=None, aspect_ratios=None,
+                 flip_aspect_ratios=True, variances=[0.1], clip=True, **kwargs):
+        #th `(samples, channels, rows, cols)`
+        if keras.image_dim_ordering() == 'th':
+            self.waxis = 3
+            self.haxis = 2
+        else:
+        #tf `(samples, rows, cols, channels`
+            self.waxis = 2
+            self.haxis = 1
 
+        self.img_dimension = img_dimension
+
+        if min_size <= 0:
+            raise Exception('min_size must be positive.')
+
+        self.min_size = min_size
+        self.max_size = max_size
+        self.aspect_ratios = [1.0]
+
+        if max_size:
+            if max_size < min_size:
+                raise Exception('max_size must be greater than min_size.')
+            self.aspect_ratios.append(1.0)
+
+        if aspect_ratios:
+            for aspect_ratio in aspect_ratios:
+                if aspect_ratio in self.aspect_ratios:
+                    continue
+                self.aspect_ratios.append(aspect_ratio)
+                if flip_aspect_ratios:
+                    self.aspect_ratios.append(1.0 / ar)
+
+        self.variances = np.array(variances)
+        self.clip = True
+
+        super(PriorBox, self).__init__(**kwargs)
+
+    def get_output_shape_for(self, shape):
+
+        prior_count = len(self.aspect_ratios)
+        width = shape[self.waxis]
+        height = shape[self.haxis]
+        num_boxes = prior_count * width * height
+
+        return (shape[0], num_boxes, 8)
+
+    def call(self, x, mask=None):
+        if hasattr(x, '_keras_shape'):
+            input_shape = x._keras_shape
+        elif hasattr(keras, 'int_shape'):
+            input_shape = keras.int_shape(x)
+
+        layer_width = input_shape[self.waxis]
+        layer_height = input_shape[self.haxis]
+
+        img_width = self.img_dimension[0]
+        img_height = self.img_dimension[1]
+
+        # define prior boxes shapes
+        box_widths = []
+        box_heights = []
+
+        for aspect_ratio in self.aspect_ratios:
+            if len(box_widths) > 0 and aspect_ratio == 1:
+                size = np.sqrt(self.min_size * self.max_size)
+                box_widths.append(size)
+                box_heights.append(size)
+
+            elif len(box_widths) == 0 and aspect_ratio == 1:
+
+                box_widths.append(self.min_size)
+                box_heights.append(self.min_size)
+
+            elif aspect_ratio != 1:
+                width_size = self.min_size * np.sqrt(aspect_ratio)
+                height_size = self.min_size / np.sqrt(aspect_ratio)
+                box_widths.append(width_size)
+                box_heights.append(height_size)
+        
+
+        box_widths = 0.5 * np.array(box_widths)
+        box_heights = 0.5 * np.array(box_heights)
+
+        # define centers of prior boxes
+        step_x = img_width / layer_width
+        step_y = img_height / layer_height
+
+        linx = np.linspace(0.5 * step_x, img_width - 0.5 * step_x,
+                           layer_width)
+
+        liny = np.linspace(0.5 * step_y, img_height - 0.5 * step_y,
+                           layer_height)
+
+        centers_x, centers_y = np.meshgrid(linx, liny)
+        centers_x = centers_x.reshape(-1, 1)
+        centers_y = centers_y.reshape(-1, 1)
+        # define xmin, ymin, xmax, ymax of prior boxes
+        
+        num_priors_ = len(self.aspect_ratios)
+        prior_boxes = np.concatenate((centers_x, centers_y), axis=1)
+        prior_boxes = np.tile(prior_boxes, (1, 2 * num_priors_))
+        prior_boxes[:, ::4] -= box_widths
+        prior_boxes[:, 1::4] -= box_heights
+        prior_boxes[:, 2::4] += box_widths
+        prior_boxes[:, 3::4] += box_heights
+        prior_boxes[:, ::2] /= img_width
+        prior_boxes[:, 1::2] /= img_height
+        prior_boxes = prior_boxes.reshape(-1, 4)
+
+        if self.clip:
+            prior_boxes = np.minimum(np.maximum(prior_boxes, 0.0), 1.0)
+        # define variances
+        num_boxes = len(prior_boxes)
+        if len(self.variances) == 1:
+            variances = np.ones((num_boxes, 4)) * self.variances[0]
+        elif len(self.variances) == 4:
+            variances = np.tile(self.variances, (num_boxes, 1))
+        else:
+            raise Exception('Must provide one or four variances.')
+        prior_boxes = np.concatenate((prior_boxes, variances), axis=1)
+        prior_boxes_tensor = keras.expand_dims(keras.variable(prior_boxes), 0)
+        if keras.backend() == 'tensorflow':
+            pattern = [tf.shape(x)[0], 1, 1]
+            prior_boxes_tensor = tf.tile(prior_boxes_tensor, pattern)
+        return prior_boxes_tensor
 
 class Normalize(Layer):
     """
@@ -42,137 +187,8 @@ class Normalize(Layer):
         self.trainable_weights = [self.gamma]
 
     def call(self, x, mask=None):
-        output = keras.l2_normalize(x, self.axis)
-        output *= self.gamma
+        output = self.gamma * keras.l2_normalize(x, self.axis)
         return output
-
-
-class PriorBox(Layer):
-    """Generate the prior boxes of designated sizes and aspect ratios.
-    # Arguments
-        img_size: Size of the input image as tuple (w, h).
-        min_size: Minimum box size in pixels.
-        max_size: Maximum box size in pixels.
-        aspect_ratios: List of aspect ratios of boxes.
-        flip: Whether to consider reverse aspect ratios.
-        variances: List of variances for x, y, w, h.
-        clip: Whether to clip the prior's coordinates
-            such that they are within [0, 1].
-    # Input shape
-        4D tensor with shape:
-        `(samples, channels, rows, cols)` if dim_ordering='th'
-        or 4D tensor with shape:
-        `(samples, rows, cols, channels)` if dim_ordering='tf'.
-    # Output shape
-        3D tensor with shape:
-        (samples, num_boxes, 8)
-    # References
-        https://arxiv.org/abs/1512.02325
-    """
-    def __init__(self, img_dimension, min_size, max_size=None, aspect_ratios=None,
-                 flip=True, variances=[0.1], clip=True, **kwargs):
-
-        if keras.image_dim_ordering() == 'th':
-            self.waxis = 3
-            self.haxis = 2
-        else:
-            self.waxis = 2
-            self.haxis = 1
-
-        self.img_dimension = img_dimension
-
-        if min_size <= 0:
-            raise Exception('min_size must be positive.')
-
-        self.min_size = min_size
-        self.max_size = max_size
-        self.aspect_ratios = [1.0]
-
-        if max_size:
-            if max_size < min_size:
-                raise Exception('max_size must be greater than min_size.')
-            self.aspect_ratios.append(1.0)
-
-        if aspect_ratios:
-            for ar in aspect_ratios:
-                if ar in self.aspect_ratios:
-                    continue
-                self.aspect_ratios.append(ar)
-                if flip:
-                    self.aspect_ratios.append(1.0 / ar)
-
-        self.variances = np.array(variances)
-        self.clip = True
-        super(PriorBox, self).__init__(**kwargs)
-
-    def get_output_shape_for(self, input_shape):
-        num_priors_ = len(self.aspect_ratios)
-        layer_width = input_shape[self.waxis]
-        layer_height = input_shape[self.haxis]
-        num_boxes = num_priors_ * layer_width * layer_height
-        return (input_shape[0], num_boxes, 8)
-
-    def call(self, x, mask=None):
-        if hasattr(x, '_keras_shape'):
-            input_shape = x._keras_shape
-        elif hasattr(keras, 'int_shape'):
-            input_shape = keras.int_shape(x)
-        layer_width = input_shape[self.waxis]
-        layer_height = input_shape[self.haxis]
-        img_width = self.img_dimension[0]
-        img_height = self.img_dimension[1]
-        # define prior boxes shapes
-        box_widths = []
-        box_heights = []
-        for ar in self.aspect_ratios:
-            if ar == 1 and len(box_widths) == 0:
-                box_widths.append(self.min_size)
-                box_heights.append(self.min_size)
-            elif ar == 1 and len(box_widths) > 0:
-                box_widths.append(np.sqrt(self.min_size * self.max_size))
-                box_heights.append(np.sqrt(self.min_size * self.max_size))
-            elif ar != 1:
-                box_widths.append(self.min_size * np.sqrt(ar))
-                box_heights.append(self.min_size / np.sqrt(ar))
-        box_widths = 0.5 * np.array(box_widths)
-        box_heights = 0.5 * np.array(box_heights)
-        # define centers of prior boxes
-        step_x = img_width / layer_width
-        step_y = img_height / layer_height
-        linx = np.linspace(0.5 * step_x, img_width - 0.5 * step_x,
-                           layer_width)
-        liny = np.linspace(0.5 * step_y, img_height - 0.5 * step_y,
-                           layer_height)
-        centers_x, centers_y = np.meshgrid(linx, liny)
-        centers_x = centers_x.reshape(-1, 1)
-        centers_y = centers_y.reshape(-1, 1)
-        # define xmin, ymin, xmax, ymax of prior boxes
-        num_priors_ = len(self.aspect_ratios)
-        prior_boxes = np.concatenate((centers_x, centers_y), axis=1)
-        prior_boxes = np.tile(prior_boxes, (1, 2 * num_priors_))
-        prior_boxes[:, ::4] -= box_widths
-        prior_boxes[:, 1::4] -= box_heights
-        prior_boxes[:, 2::4] += box_widths
-        prior_boxes[:, 3::4] += box_heights
-        prior_boxes[:, ::2] /= img_width
-        prior_boxes[:, 1::2] /= img_height
-        prior_boxes = prior_boxes.reshape(-1, 4)
-        if self.clip:
-            prior_boxes = np.minimum(np.maximum(prior_boxes, 0.0), 1.0)
-        # define variances
-        num_boxes = len(prior_boxes)
-        if len(self.variances) == 1:
-            variances = np.ones((num_boxes, 4)) * self.variances[0]
-        elif len(self.variances) == 4:
-            variances = np.tile(self.variances, (num_boxes, 1))
-        else:
-            raise Exception('Must provide one or four variances.')
-        prior_boxes = np.concatenate((prior_boxes, variances), axis=1)
-        prior_boxes_tensor = keras.expand_dims(keras.variable(prior_boxes), 0)
-        if keras.backend() == 'tensorflow':
-            pattern = [tf.shape(x)[0], 1, 1]
-            prior_boxes_tensor = tf.tile(prior_boxes_tensor, pattern)
-        return prior_boxes_tensor
 
 
 def SSD300(input_shape, num_classes=21):
@@ -317,6 +333,8 @@ def SSD300(input_shape, num_classes=21):
     net['conv4_3_norm_mbox_conf'] = x
     flatten = Flatten(name='conv4_3_norm_mbox_conf_flat')
     net['conv4_3_norm_mbox_conf_flat'] = flatten(net['conv4_3_norm_mbox_conf'])
+
+
     priorbox = PriorBox(img_size, 30.0, aspect_ratios=[2],
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv4_3_norm_mbox_priorbox')
@@ -417,6 +435,7 @@ def SSD300(input_shape, num_classes=21):
     net['pool6_reshaped'] = Reshape(target_shape,
                                     name='pool6_reshaped')(net['pool6'])
     net['pool6_mbox_priorbox'] = priorbox(net['pool6_reshaped'])
+
     # Gather all predictions
     net['mbox_loc'] = merge([net['conv4_3_norm_mbox_loc_flat'],
                              net['fc7_mbox_loc_flat'],
@@ -440,6 +459,7 @@ def SSD300(input_shape, num_classes=21):
                                   net['pool6_mbox_priorbox']],
                                  mode='concat', concat_axis=1,
                                  name='mbox_priorbox')
+
     if hasattr(net['mbox_loc'], '_keras_shape'):
         num_boxes = net['mbox_loc']._keras_shape[-1] // 4
     elif hasattr(net['mbox_loc'], 'int_shape'):
@@ -455,5 +475,6 @@ def SSD300(input_shape, num_classes=21):
                                net['mbox_priorbox']],
                                mode='concat', concat_axis=2,
                                name='predictions')
+
     model = Model(net['input'], net['predictions'])
     return model
