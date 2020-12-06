46916a882c5e3176a624fbe7b7af4b330275b81c
Stefan Philip <stefankphilip631@gmail.com>
2020-12-05 19:58:50 -0500

    vgg architecture
    base model (pretrained)

--- fasterRCNN/faster_rcnn.py
+++ fasterRCNN/faster_rcnn.py
@@ -65,30 +65,28 @@ def iou(a, b):
     return float(area_i) / float(area_u + 1e-6)
 
 
-
 # Creating a Region of Interest (ROI) pooling layer for 2D inputs
 # Spatial Pyramid Pooling in Deep Conv. Nets paper
 class RoiPooling2D(Layer):
 
     def __init__(self, pool_size, num_rois, **kwargs):
-        self.dim_ordering = K.image_dim_ordering() # "tf", using TensorFlow backend
+        self.dim_ordering = K.image_dim_ordering()  # "tf", using TensorFlow backend
         self.pool_size = pool_size
         self.num_rois = num_rois
 
         super(RoiPooling2D, self).__init__(**kwargs)
 
-    def build (self, input_shape):
+    def build(self, input_shape):
         self.nb_channels = input_shape[0][3]
 
     def compute_output_shape(self, input_shape):
         return None, self.num_rois, self.pool_size, self.pool_size, self.nb_channels
 
     def call(self, x, mask=None):
+        assert (len(x) == 2)
 
-        assert(len(x) == 2)
-
-        img = x[0] # image with (rows, cols, channels) as shape
-        rois = x[1] # roi with (num_rois, 4) as shape, ordering is (x, y, w, h)
+        img = x[0]  # image with (rows, cols, channels) as shape
+        rois = x[1]  # roi with (num_rois, 4) as shape, ordering is (x, y, w, h)
 
         input_shape = K.shape(img)
         outputs = []
@@ -104,7 +102,7 @@ class RoiPooling2D(Layer):
             outputs.append(resized)
 
         # reshape to (1, num_rois, pool size, pool size, # of channels)
-        final_out = K.concatenate(outputs, axis = 0)
+        final_out = K.concatenate(outputs, axis=0)
         final_out = K.reshape(final_out, (1, self.num_rois, self.pool_size, self.pool_size, self.nb_channels))
         final_out = K.permute_dimensions(final_out, (0, 1, 2, 3, 4))
 
@@ -114,4 +112,49 @@ class RoiPooling2D(Layer):
         config = {"pool_size": self.pool_size,
                   "num_rois": self.num_rois}
         base_config = super(RoiPooling2D, self).get_config()
-        return dict( list(base_config.items()) + list(config.items()) )
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+# Creating the vgg16 model
+# based on vgg16 model architecture
+def vgg_base(input_tensor=None, trainable=False):
+    input_shape = (None, None, 3)
+
+    # validate the input tensor and make input layer
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    # 1 Block
+    x = Conv2D(64, (3, 3), activation="relu", padding="same", name="block1_conv1")(img_input)
+    x = Conv2D(64, (3, 3), activation="relu", padding="same", name="block1_conv2")(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name="block1_pool")(x)
+
+    # 2 Block
+    x = Conv2D(128, (3, 3), activation="relu", padding="same", name="block2_conv1")(x)
+    x = Conv2D(128, (3, 3), activation="relu", padding="same", name="block2_conv2")(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name="block2_pool")(x)
+
+    # 3 Block
+    x = Conv2D(256, (3, 3), activation="relu", padding="same", name="block3_conv1")(x)
+    x = Conv2D(256, (3, 3), activation="relu", padding="same", name="block3_conv2")(x)
+    x = Conv2D(256, (3, 3), activation="relu", padding="same", name="block3_conv3")(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name="block3_pool")(x)
+
+    # 4 Block
+    x = Conv2D(256, (3, 3), activation="relu", padding="same", name="block4_conv1")(x)
+    x = Conv2D(256, (3, 3), activation="relu", padding="same", name="block4_conv2")(x)
+    x = Conv2D(256, (3, 3), activation="relu", padding="same", name="block4_conv3")(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name="block4_pool")(x)
+
+    # 5 Block
+    x = Conv2D(512, (3, 3), activation="relu", padding="same", name="block5_conv1")(x)
+    x = Conv2D(512, (3, 3), activation="relu", padding="same", name="block5_conv2")(x)
+    x = Conv2D(512, (3, 3), activation="relu", padding="same", name="block5_conv3")(x)
+
+    return x
+
