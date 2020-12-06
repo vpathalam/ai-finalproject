ff1f77f2dd2f34925a1d873652fb2140ea61b139
Stefan Philip <stefankphilip631@gmail.com>
2020-12-05 21:04:06 -0500

    loss functions

--- fasterRCNN/faster_rcnn.py
+++ fasterRCNN/faster_rcnn.py
@@ -9,6 +9,17 @@ from keras.engine import Layer
 from keras import backend as K, Input
 import tensorflow as tf
 from keras.layers import Conv2D, MaxPooling2D, TimeDistributed, Flatten, Dense, Dropout
+from keras.objectives import categorical_crossentropy
+
+
+# Hyperparameters for Loss functions
+lambda_rpn_regr = 1.0
+lambda_rpn_class = 1.0
+
+lambda_cls_regr = 1.0
+lambda_cls_class = 1.0
+
+epsilon = 0.0001
 
 
 class Config:
@@ -526,4 +537,46 @@ def get_anchor_gt(all_img_data, C, img_length_calc_function, mode='train'):
             except Exception as e:
                 print(e)
                 continue
+
+
+# loss function for RPN regression
+def rpn_loss_regr(num_anchors):
+    def rpn_loss_regr_fixed_num(y_true, y_pred):
+
+        # x is the difference between true value and predicted vaue
+        x = y_true[:, :, :, 4 * num_anchors:] - y_pred
+
+        # absolute value of x
+        x_abs = K.abs(x)
+
+        # If x_abs <= 1.0, x_bool = 1
+        x_bool = K.cast(K.less_equal(x_abs, 1.0), tf.float32)
+
+        return lambda_rpn_regr * K.sum(
+            y_true[:, :, :, :4 * num_anchors] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / K.sum(epsilon + y_true[:, :, :, :4 * num_anchors])
+
+    return rpn_loss_regr_fixed_num
+
+# loss function for RPN classification
+def rpn_loss_cls(num_anchors):
+    def rpn_loss_cls_fixed_num(y_true, y_pred):
+
+            return lambda_rpn_class * K.sum(y_true[:, :, :, :num_anchors] * K.binary_crossentropy(y_pred[:, :, :, :], y_true[:, :, :, num_anchors:])) / K.sum(epsilon + y_true[:, :, :, :num_anchors])
+
+    return rpn_loss_cls_fixed_num
+
+
+# loss function for RPN regression (classes)
+def class_loss_regr(num_classes):
+    def class_loss_regr_fixed_num(y_true, y_pred):
+        x = y_true[:, :, 4*num_classes:] - y_pred
+        x_abs = K.abs(x)
+        x_bool = K.cast(K.less_equal(x_abs, 1.0), 'float32')
+        return lambda_cls_regr * K.sum(y_true[:, :, :4*num_classes] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / K.sum(epsilon + y_true[:, :, :4*num_classes])
+    return class_loss_regr_fixed_num
+
+
+# loss function for RPN classification (classes)
+def class_loss_cls(y_true, y_pred):
+    return lambda_cls_class * K.mean(categorical_crossentropy(y_true[0, :, :], y_pred[0, :, :]))
 
